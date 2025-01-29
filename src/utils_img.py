6ab3ca882ed91305c19d26d759255471cf46f367
Asilvorcarp <asilvorcarp@qq.com>
2025-01-30 03:05:25 +0800

    fix license

--- src/utils_img.py
+++ src/utils_img.py
@@ -1,11 +1,3 @@
-# Copyright (c) Meta Platforms, Inc. and affiliates.
-# All rights reserved.
-
-# This source code is licensed under the license found in the
-# LICENSE file in the root directory of this source tree.
-
-# pyright: reportMissingModuleSource=false
-
 import warnings
 warnings.filterwarnings("ignore")
 import numpy as np
